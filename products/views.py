f35742d64da2a48b8ccf972b3cf70c88fa5c5ac0
Michael O'Farrell <mofarrell1967@gmail.com>
2020-04-04 09:54:42 +0000

    Added initial views to products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render
+from .models import Product
+
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, "products.html", {"products": products})
